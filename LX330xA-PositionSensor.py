63975dc545e15786f539e8987797ae429787533a
Mark <drkmsmithjr@gmail.com>
2018-10-04 05:17:53 +0000

    Implemented a filter for the PWM output

--- LX330xA-PositionSensor.py
+++ LX330xA-PositionSensor.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 import math
 import time
 import threading
+import sys
 
 # This will define a Position Sensor Class Object   
 # Part numbers Supported:
@@ -37,6 +38,14 @@ class PositionSensor():
             self.pins = IO2
 		
         self.data = 0
+        self.status = 0
+        self.chan1 = 0
+        self.chan2 = 0
+        self.chan3 = 0
+        self.chan4 = 0
+        self.chan5 = 0
+        self.chan6 = 0
+        self.crc = 0
         self.PWMFreq = 0
         self.Arg1 = self.pins
         self.Arg2 = 0
@@ -71,21 +80,41 @@ class PositionSensor():
         while True:
            # read the Status nibble
            # measure the pulse width of each period
-           self.DetectNibble(Arg1)
-           
-		   
-   
+           self.data = self.DetectNibble(Arg1)
+           while self.data > 40:
+              # get status nibble
+              self.status = self.DetectNibble(Arg1)
+              self.chan1 = self.DetectNibble(Arg1)
+              self.chan2 = self.DetectNibble(Arg1)
+              self.chan3 = self.DetectNibble(Arg1)
+              self.chan4 = self.DetectNibble(Arg1)
+              self.chan5 = self.DetectNibble(Arg1)
+              self.chan6 = self.DetectNibble(Arg1)
+              self.crc = self.DetectNibble(Arg1)           
+	   
     def PWMSample(self,Arg1,Arg2):
         
         # this will run in a loop and sample the PWM path
         # the algorithm will read leading edge PWM.  the
         # states:   The output is high,  wait until the output goes low.
         # states:   the output is low, wait until the output goes high.
-        # An error occurs if delay too much that we had to break out of loop.   		
+        # An error occurs if delay too much that we had to break out of loop.  
+
+        # inplemented a LPF for the data
+        # also implemented an outlier filter for erroneous data 		
         INPUT = GPIO.input(Arg1)
         StartPeriod = time.time()
         PWMDuty = 0
         PWMFreq = 0
+
+        # low pass filter 10 taps
+        LPF = [0]*10
+        tempval = 0
+        tempval2 = 1
+        lastPWMFreq = 1
+        outliers = 0.05
+
+
         while True:
             INPUT = GPIO.input(Arg1)
             # read the PWM signal
@@ -103,10 +132,26 @@ class PositionSensor():
             StartPeriod = StopPeriod
 			#clock frequency to nearest Hertz
             self.PWMFreq = math.floor(1/ClockPeriod+.5)
-						
+            
             PulseWidth = PulseFallEdge - PulseRiseEdge
-			# duty ratio with two digits
-            self.data = (math.floor(PulseWidth/ClockPeriod*10000+.5))/100.0			
+            if abs(1.0*self.PWMFreq/lastPWMFreq -1)< outliers:
+               tempval = (math.floor(PulseWidth/ClockPeriod*10000+.5))/100.0
+               if abs(1.0*tempval/tempval2 - 1) < outliers:
+                  LPF.pop()
+                  LPF.insert(0,tempval)
+                  indexval = len(LPF)
+                  tempval2 = 0
+                  for x in LPF:
+                     tempval2 += (1.0/indexval)*x
+                  self.data = tempval2
+               tempval2 = tempval
+            #else:
+            #   tempval = tempval2
+
+            lastPWMFreq = self.PWMFreq
+               # duty ratio with two digits
+            #tempval = (math.floor(PulseWidth/ClockPeriod*10000+.5))/100.0
+            
 		                
     def Get_Data(self):        
         return self.data 
@@ -123,26 +168,38 @@ class PositionSensor():
 
         while INPUT == False:
             INPUT = GPIO.input(Arg1)
-	    PulseRiseEdge = time.time()
+        PulseRiseEdge = time.time()
 
 	while INPUT == True:
             INPUT = GPIO.input(Arg1)
-            PulseFallEdge = time.time()
+        PulseFallEdge = time.time()
 
         Value = math.floor((PulseFallEdge - PulseRiseEdge)/ClockTick+.5)-7 
-        self.data = Value
+        #self.data = Value
         return Value
 
 		
-		
+Type = 'PWM'	
   
 if __name__ == "__main__":
-        print "hello world"
-        #x = PositionSensor('LX3302A','SENT','IO3')
-        y = PositionSensor('LX3302A','PWM','IO2')
+   
+   if len(sys.argv) >= 2:
+      if sys.argv[1] == 'SENT':
+         x = PositionSensor('LX3302A','SENT','IO3')
+         Type = 'SENT'
+      else:
+         x = PositionSensor('LX3302A','PWM','IO2')
+         Type = 'PWM'
+   else:
+      x = PositionSensor('LX3302A','PWM','IO2')
+      Type = 'PWM'
+
+   print "hello world"
         
-        while True:
-            #print("Nibble Value: %s " % int(x.Get_Data()))
-            print("Duty Ration: %s    Frequency: %s" % (int(y.Get_Data()),int(y.Get_Freq())))            
-            time.sleep(.01)
+   while True:
+      if Type == 'SENT':
+         print("Start Lenth: %s Status: %s Data1: %s-%s-%s Data2: %s-%s-%s CRC: %s " % (int(x.Get_Data()),int(x.status),int(x.chan1),int(x.chan2),int(x.chan3),int(x.chan4),int(x.chan5),int(x.chan6),int(x.crc)))
+      else:
+         print("Duty Ration: %s    Frequency: %s" % (int(x.Get_Data()),int(x.Get_Freq())))            
+      time.sleep(.01)
            
