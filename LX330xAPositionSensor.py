68d35792f9e7e5fa71276bc51de7e2ac62c9127c
Mark <drkmsmithjr@gmail.com>
2020-11-26 05:10:52 +0000

    added a dataSENTEASY format to collect data and airgap from the -EASY sensors

--- LX330xAPositionSensor.py
+++ LX330xAPositionSensor.py
@@ -19,29 +19,24 @@ SENT = 'SENT'
 # PWM
 PWW = 'PWM'
 
-# BCM GPIO pins to connect to the Sensor
+# Default BCM GPIO pins to connect to the Sensor
 IO1 = 23
-IO2_DOUT = 18
+IO2_DOUT = 25
 IO3_AOUT = 24
+IO4 = 12
 
 class PositionSensor():
 
-    def __init__(self,partnumber = 'LX3302A', protocol = 'SENT', pin = 'IO3'):
+    def __init__(self,partnumber = 'LX3302A', protocol = 'SENT', pin = IO3_AOUT):
 
         # start a pi object
         pi = pigpio.pi()
 
+        self.fault = False
+
         self.partnumber = partnumber
         self.protocol = protocol
-        if (pin == 'IO3') or (pin == 'AOUT'):
-           self.pins = IO3_AOUT
-        elif (pin == 'IO2') or (pin == 'DOUT'):
-           self.pins = IO2_DOUT
-        elif pin == 'IO1':
-           self.pins = IO1
-        else:
-           print("No correct pin detected... this is an error")
-
+        self.pins = pin
         # start the thread to sample output depending on the output
         if protocol == 'SENT':
            print("SENT protocol")
@@ -59,8 +54,25 @@ class PositionSensor():
 
     def dataSENT(self):
         if self.protocol == 'SENT':
-            # only return the first data
-            return self.SENT.dataField1()
+            # sample until SENTData fault but return after 10 tries
+            fault = True
+            while (fault == True):
+                SENTStatus, returnData, returnData2,SENTTick, SENTCrc, fault, SENTPeriod = self.SENT.SENTData()
+            return returnData
+        else:
+            return 0
+
+    def dataSENTEasy(self):
+        # read the sent channel for the LX3302AQPW-EASY
+        # this channel has position plus airgap information
+        if (self.protocol == 'SENT') and (self.partnumber=='LX3302A'):
+            # sample until SENTData fault but return after 10 tries
+            fault = True
+            while (fault == True):
+                SENTStatus, returnData, returnData2,SENTTick, SENTCrc, fault, SENTPeriod = self.SENT.SENTData()
+            return returnData, returnData2
+        else:
+            return 0, 0
 
     def PWMFreq(self):
         if self.protocol == 'PWM':
@@ -85,11 +97,13 @@ Type = ''
 
 if __name__ == "__main__":
 
-   # read two outptus from the LX3302A, both IO2 and IO3 as follows:
-   x = PositionSensor('LX3302A','SENT','IO2')
+   # read two outptus from the LX3302A and connect to the BCD GPIO pin
+   x = PositionSensor('LX3302A','SENT', 18)
    #y = PositionSensor('LX3302A','PWM','IO3')
 
    while True:
          #print("Duty Ratio: %s SENT_duty: %s PWM Frequency: %s SENT: %s " % (round(y.DutyRatio(),2),(round(x.dataSENT()/4096.0*100,2)),round(y.PWMFreq(),2),x.dataSENT()))
-         print("SENT_duty: %s SENT: %s " % ((round(x.dataSENT()/4096.0*100,2)),x.dataSENT()))
-         time.sleep(0.1)
+         #print("SENT_duty: %s SENT: %s " % ((round(x.dataSENT()/4096.0*100,2)),x.dataSENT()))
+        data, airgap = x.dataSENTEasy()
+        print("SENT = %s AIRGAP = %s" % (data,airgap))
+        time.sleep(0.1)
